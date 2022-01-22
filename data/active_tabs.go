91b1020e546840e4bc540abec7be4ebf3a7726ea
nadir akdağ <nadirakdag@outlook.com>
2022-01-22 13:35:22 +0300

    added comments active tabs repository

--- data/active_tabs.go
+++ data/active_tabs.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 )
 
+// active tab repository interface
 type ActiveTabsRepository interface {
 	GetAll() []models.ActiveTab
 	Get(key string) (*models.ActiveTab, error)
@@ -14,20 +15,27 @@ type ActiveTabsRepository interface {
 var ErrActiveTabExists = errors.New("active tab key exists")
 var ErrActiveTabNotFound = errors.New("active tab does not exits")
 
+// active tabs in-memory database repository for active tabs
 type ActiveTabsInMemoryRepository struct {
 	ActiveTabs []models.ActiveTab
 }
 
+// creates a new in-memory database repository for active tabs
 func NewActiveTabsInMemoryRepository() ActiveTabsRepository {
 	return &ActiveTabsInMemoryRepository{
 		ActiveTabs: []models.ActiveTab{},
 	}
 }
 
+// implements GetAll method from ActiveTabsRepository for in-memory database
+// returns all active tabs from in-memory database
 func (activeTab ActiveTabsInMemoryRepository) GetAll() []models.ActiveTab {
 	return activeTab.ActiveTabs
 }
 
+// implements Get method from ActiveTabsRepository for in-memory database
+// returns an active tab that request with key from in-memory database
+// if tab does not exists then returns ErrActiveTabNotFound
 func (activeTab ActiveTabsInMemoryRepository) Get(key string) (*models.ActiveTab, error) {
 
 	for _, item := range activeTab.ActiveTabs {
@@ -39,6 +47,9 @@ func (activeTab ActiveTabsInMemoryRepository) Get(key string) (*models.ActiveTab
 	return nil, ErrActiveTabNotFound
 }
 
+// implements Add method from ActiveTabsRepository for in-memory database
+// trys to add new active tab to in-memory database
+// if tab already exists returns ErrActiveTabExists error
 func (activeTab *ActiveTabsInMemoryRepository) Add(newActiveTab models.ActiveTab) error {
 	tab, _ := activeTab.Get(newActiveTab.Key)
 	if tab != nil {
