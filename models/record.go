237228e3195fa072b609590d7aa1c57cac5c4ba5
nadir akdağ <nadirakdag@outlook.com>
2022-01-22 13:43:03 +0300

    added comment to Record model

--- models/record.go
+++ models/record.go
@@ -2,6 +2,7 @@ package models
 
 import "time"
 
+// record database entity model
 type Record struct {
 	Key        string    `json:"key" bson:"key"`
 	CreatedAt  time.Time `json:"createdAt" bson:"createdAt"`
