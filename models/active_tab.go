8607adb5a4a062b2f9bac8557f9397b3f6413aa2
nadir akdağ <nadirakdag@outlook.com>
2022-01-22 13:41:31 +0300

    added comments for active tab model

--- models/active_tab.go
+++ models/active_tab.go
@@ -5,21 +5,26 @@ import (
 	"io"
 )
 
+// activetab entity model
 type ActiveTab struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
 
+// converts JSON to activeTab struct
 func (activeTab *ActiveTab) FromJSON(r io.Reader) error {
 	return json.NewDecoder(r).Decode(activeTab)
 }
 
+//converts activeTab to JSON
 func (activeTab *ActiveTab) ToJSON(w io.Writer) error {
 	return json.NewEncoder(w).Encode(activeTab)
 }
 
+// activeTab Array model
 type ActiveTabs []ActiveTab
 
+// converts activeTabs array to JSON
 func (activeTabs ActiveTabs) ToJSON(w io.Writer) error {
 	return json.NewEncoder(w).Encode(activeTabs)
 }
